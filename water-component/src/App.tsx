fb10d9c50cb295ceb965ee058329f681be50494e
刘鹏 <2254842466@qq.com>
2024-08-05 14:41:08 +0800

    done

---
+++ water-component/src/App.tsx
@@ -0,0 +1,54 @@
+import Watermark from "./Watermark";
+
+const App = () => {
+  return (
+    <Watermark content={["测试水印", "神说要有光"]}>
+      <div style={{ height: 800 }}>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quod
+          deserunt quidem quas in rem ipsam ut nesciunt asperiores dignissimos
+          recusandae minus, eaque, harum exercitationem esse sapiente? Eveniet,
+          id provident!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quod
+          deserunt quidem quas in rem ipsam ut nesciunt asperiores dignissimos
+          recusandae minus, eaque, harum exercitationem esse sapiente? Eveniet,
+          id provident!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quod
+          deserunt quidem quas in rem ipsam ut nesciunt asperiores dignissimos
+          recusandae minus, eaque, harum exercitationem esse sapiente? Eveniet,
+          id provident!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quod
+          deserunt quidem quas in rem ipsam ut nesciunt asperiores dignissimos
+          recusandae minus, eaque, harum exercitationem esse sapiente? Eveniet,
+          id provident!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quod
+          deserunt quidem quas in rem ipsam ut nesciunt asperiores dignissimos
+          recusandae minus, eaque, harum exercitationem esse sapiente? Eveniet,
+          id provident!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quod
+          deserunt quidem quas in rem ipsam ut nesciunt asperiores dignissimos
+          recusandae minus, eaque, harum exercitationem esse sapiente? Eveniet,
+          id provident!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos quod
+          deserunt quidem quas in rem ipsam ut nesciunt asperiores dignissimos
+          recusandae minus, eaque, harum exercitationem esse sapiente? Eveniet,
+          id provident!
+        </p>
+      </div>
+    </Watermark>
+  );
+};
+
+export default App;
